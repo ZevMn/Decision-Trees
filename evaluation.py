ddb0be13ea446857d968d7ce2d73bef70ad364f2
Lucy Armitage <la724@imperial.ac.uk>
2025-02-07 13:15:45 +0000

    remove savefig

--- evaluation.py
+++ evaluation.py
@@ -187,7 +187,6 @@ def plot_confusion_matrix(confusion, class_labels, title="Confusion Matrix"):
     plt.title(f"Confusion Matrix {title}", fontsize=20, pad=20)
     plt.xlabel("Predicted", fontsize=14)
     plt.ylabel("Actual", fontsize=14)
-    plt.savefig(f"Confusion_{title}.png")
     plt.show()
 
 def plot_metrics(y_gold, y_prediction, confusion, class_labels=None, title="Precision, Recall, and F1 Score per Class"):
@@ -228,7 +227,6 @@ def plot_metrics(y_gold, y_prediction, confusion, class_labels=None, title="Prec
     plt.yticks(fontsize=12)
     plt.legend()
     plt.grid()
-    plt.savefig(f"Eval_{title}.png")
     plt.tight_layout()
     plt.show()
 
